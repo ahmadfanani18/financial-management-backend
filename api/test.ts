2bb759c9b670e3bc50219022a5d1f5309e6d9fc4
Ahmad Fanani <fananiapps@gmail.com>
2026-05-05 11:07:46 +0700

    test: add logging

--- api/test.ts
+++ api/test.ts
@@ -1,5 +1,9 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+  console.error('Test handler called');
+  console.error('req:', typeof req, req.url);
+  console.error('res:', typeof res);
+  
   res.status(200).json({ message: 'Hello from Vercel!' });
 }
