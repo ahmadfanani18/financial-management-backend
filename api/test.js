3e302121cf93fde8a6e0b1bf1b9d15b1449010ad
Ahmad Fanani <fananiapps@gmail.com>
2026-05-05 11:05:13 +0700

    test: async handler format

--- api/test.js
+++ api/test.js
@@ -1,3 +1,3 @@
-module.exports = (req, res) => {
-  res.json({ message: 'Hello from Vercel!' });
+module.exports = async function(req, res) {
+  return res.status(200).json({ message: 'Hello from Vercel!' });
 };
