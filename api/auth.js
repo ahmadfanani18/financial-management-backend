811d377541a60c4c00d575a067b652e3e12ba7d9
Ahmad Fanani <fananiapps@gmail.com>
2026-05-07 09:03:14 +0700

    fix: add missing verifyPassword import

--- api/auth.js
+++ api/auth.js
@@ -1,4 +1,4 @@
-import { getPrisma, parseBody, simpleToken, setupCors, hashPassword } from './utils.js';
+import { getPrisma, parseBody, simpleToken, setupCors, hashPassword, verifyPassword } from './utils.js';
 
 export default async function handler(req, res) {
   const origin = req.headers.origin;
